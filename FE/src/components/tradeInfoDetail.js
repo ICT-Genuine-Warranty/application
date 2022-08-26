638bc4f2e6443300c0cd28c65282a20d2ff32c0b
donggni0712 <donggni0712@naver.com>
2022-08-26 17:05:49 +0900

    :hammer:Finish

--- FE/src/components/tradeInfoDetail.js
+++ FE/src/components/tradeInfoDetail.js
@@ -1,4 +1,7 @@
+import {useEffect} from 'react'
+
 function TradeInfoDetail({data,setIsDetail}){
+    console.log(`http://localhost:3065/image/getimg/${data.comments.sellersComment.commentURI}`)
     return(
         <div>
             <div style={{ width:"80px", marginBottom:"10px",paddingLeft:"5px",paddingRight:"5px", borderRadius:'5px', marginLeft:'420px', marginRight:'20px', backgroundColor:'#E9E9E9', color:'#888888', fontSize:"small"}}
@@ -13,12 +16,14 @@ function TradeInfoDetail({data,setIsDetail}){
                 <div>거래 날짜:{data.traded}</div>
             </div>
             <div style={{ marginTop:"30px", backgroundColor:"#E9E9E9", width:'500px', borderRadius:'5px'}}>
-                <div>판매자 : {data.comments.sellersComment.owner}</div>
-                <div>{data.comments.sellersComment.commentURI}</div>
+                <div style={{ marginLeft:"10px",  display:"flex", fontWeight:"bold"}}>판매자 : {data.comments.sellersComment.owner}</div>
+                <div style={{ marginLeft:"10px",  display:"flex"}}> {data.comments.sellersComment.content}</div>
+                <div><img style={{marginBottom:"30px", width:"300px"}} src={`http://localhost:3065/image/getimg/${data.comments.sellersComment.imgSrc}`}/></div>
             </div>
             <div style={{ marginTop:"30px", backgroundColor:"#E9E9E9", width:'500px', borderRadius:'5px'}}>
-                <div>구매자 : {data.comments.buyersComment.owner}</div>
-                <div>{data.comments.buyersComment.commentURI}</div>
+                <div style={{ marginLeft:"10px",  display:"flex", fontWeight:"bold"}}>구매자 : {data.comments.buyersComment.owner}</div>
+                <div style={{ marginLeft:"10px",  display:"flex"}}> {data.comments.buyersComment.content}</div>
+                <div><img style={{marginBottom:"30px", width:"300px"}} src={`http://localhost:3065/image/getimg/${data.comments.buyersComment.imgSrc}`}/></div>
             </div>
         </div>
     )
